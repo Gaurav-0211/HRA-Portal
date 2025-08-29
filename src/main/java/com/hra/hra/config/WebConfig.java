edf27c87e328bb522a2aa02d9bec06ca05cc8a33
Gaurav Kumar <gauravkr0211@gmail.com>
2025-08-29 16:02:19 +0530

    web config updated

---
+++ src/main/java/com/hra/hra/config/WebConfig.java
@@ -0,0 +1,38 @@
+package com.hra.hra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// This class is used to connect Frontend with the Backend Cors allow to send data and fetch Backend API's
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+
+        };
+    }
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .favorPathExtension(false)
+                .favorParameter(false)
+                .ignoreAcceptHeader(true);
+    }
+}
+
+
