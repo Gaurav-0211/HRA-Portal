ec23e06e57163d628fe699bb755fce27576b85c1
Gaurav Kumar <gauravkr0211@gmail.com>
2026-01-07 18:53:39 +0530

    _Fix: Leave updated

--- src/main/java/com/hra/hra/config/FiscalYearUtil.java
+++ src/main/java/com/hra/hra/config/FiscalYearUtil.java
@@ -3,9 +3,9 @@ package com.hra.hra.config;
 import java.time.LocalDate;
 
 public class FiscalYearUtil {
-    /**
-     * returns "YYYY-YYYY" representing fiscal year Apr1..Mar31 that contains given date.
-     */
+
+     // Returns "YYYY-YYYY" representing fiscal year Apr1 t0 Mar31 that contains given date.
+
     public static String fiscalYearFor(LocalDate date) {
         int year = date.getYear();
         if (date.getMonthValue() >= 4) {
